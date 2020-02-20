649803f5b40511a2b0a3607937a389713b4fe4f4
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-02-19 21:00:15 -0500

    dependencies

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
